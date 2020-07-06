758be2bf6e22771cb6deaba8e175ed4723cc169c
Chioma Ubogagugit config --global --listexit <cubogagu10@gmail.com>
2020-07-06 16:46:43 -0500

    Delete Commit

--- beltExams/pythonBeltExam/beltExamApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BeltexamappConfig(AppConfig):
-    name = 'beltExamApp'
--- beltExams/rubyBeltExam/db/migrate/20200618182603_remove_password_confirmation_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemovePasswordConfirmationFromUser < ActiveRecord::Migration
-  def change
-    remove_column :users, :passwordConfirmation, :string
-  end
-end
